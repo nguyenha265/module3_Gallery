d6b166478579c35bf9b3e111c6acddd5956517ce
anhhaday <nguyenha26051988@gmail.com>
2019-10-31 11:04:02 +0700

    thuc hanh

---
+++ src/app/image-gallery/image-gallery.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {ImageCardComponent} from './image-card/image-card.component';
+import {ImageGalleryComponent} from './image-gallery/image-gallery.component';
+
+
+
+@NgModule({
+  declarations: [ImageCardComponent, ImageGalleryComponent],
+  exports: [ImageGalleryComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ImageGalleryModule { }
